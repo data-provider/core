ed178ff3ca430c40620254f84261602f73147e41
javierbrea <javier.brea@gmail.com>
2019-06-11 07:59:08 +0200

    Do not execute apis tests for debugging purposes

--- jest.config.js
+++ jest.config.js
@@ -28,7 +28,17 @@ module.exports = {
   testEnvironment: "node",
 
   // The glob patterns Jest uses to detect test files
-  testMatch: ["**/test/**/?(*.)+(spec|test).js?(x)"],
+  //testMatch: ["**/test/**/?(*.)+(spec|test).js?(x)"],
+  testMatch: [
+    "**/test/config.spec.js",
+    "**/test/custom-queries.spec.js",
+    "**/test/events.spec.js",
+    "**/test/headers.spec.js",
+    "**/test/index.spec.js",
+    "**/test/methods.spec.js",
+    "**/test/queries.spec.js",
+    "**/test/selectors.spec.js"
+  ],
 
   transform: {
     ".js$": "babel-jest"
