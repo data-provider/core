1900cd786e675f4df004837893c7e7ca98349b5b
javierbrea <javier.brea@gmail.com>
2019-10-14 12:06:56 +0200

    Add stats unit tests for queried sources

--- test/Origin.stats.js
+++ test/Origin.stats.js
@@ -2,10 +2,11 @@ const test = require("mocha-sinon-chai");
 
 const { Origin, sources } = require("../src/Origin");
 
-test.describe("Origin stats property", () => {
+test.describe("Origin stats getters", () => {
   let sandbox;
   let TestOrigin;
   let testOrigin;
+  let queriedOrigin;
   let hasToReject;
 
   test.beforeEach(() => {
@@ -27,6 +28,7 @@ test.describe("Origin stats property", () => {
       }
     };
     testOrigin = new TestOrigin("foo-id");
+    queriedOrigin = testOrigin.query("foo");
   });
 
   test.afterEach(() => {
@@ -34,7 +36,7 @@ test.describe("Origin stats property", () => {
     sources.clear();
   });
 
-  test.describe("using getter", () => {
+  test.describe("without query", () => {
     test.describe("dispatch property", () => {
       test.it("should return 0 until read is dispatched", () => {
         test.expect(testOrigin.read.getters.stats().dispatch).to.equal(0);
@@ -124,6 +126,103 @@ test.describe("Origin stats property", () => {
             },
             () => {
               return test.expect(testOrigin.read.getters.stats().error).to.equal(4);
+            }
+          );
+        }
+      );
+    });
+  });
+
+  test.describe("with query", () => {
+    test.describe("dispatch property", () => {
+      test.it("should return 0 until read is dispatched", () => {
+        test.expect(queriedOrigin.read.getters.stats().dispatch).to.equal(0);
+      });
+
+      test.it("should return 1 when read is dispatched", () => {
+        queriedOrigin.read();
+        test.expect(queriedOrigin.read.getters.stats().dispatch).to.equal(1);
+      });
+
+      test.it(
+        "should return 1 when read is dispatched many times and cache is not cleaned",
+        () => {
+          queriedOrigin.read();
+          queriedOrigin.read();
+          queriedOrigin.read();
+          queriedOrigin.read();
+          test.expect(queriedOrigin.read.getters.stats().dispatch).to.equal(1);
+        }
+      );
+
+      test.it(
+        "should return 2 when read is dispatched many times and cache is cleaned once",
+        () => {
+          queriedOrigin.read();
+          queriedOrigin.clean();
+          queriedOrigin.read();
+          queriedOrigin.read();
+          queriedOrigin.read();
+          test.expect(queriedOrigin.read.getters.stats().dispatch).to.equal(2);
+        }
+      );
+    });
+
+    test.describe("success property", () => {
+      test.it("should return 0 until read method has finished", () => {
+        test.expect(queriedOrigin.read.getters.stats().success).to.equal(0);
+      });
+
+      test.it("should return 1 when read has finished", () => {
+        return queriedOrigin.read().then(() => {
+          return test.expect(queriedOrigin.read.getters.stats().success).to.equal(1);
+        });
+      });
+
+      test.it("should return 4 when read has finished and it has been called 4 times", () => {
+        return Promise.all([
+          queriedOrigin.read(),
+          queriedOrigin.read(),
+          queriedOrigin.read(),
+          queriedOrigin.read()
+        ]).then(() => {
+          return test.expect(queriedOrigin.read.getters.stats().success).to.equal(4);
+        });
+      });
+    });
+
+    test.describe("error property", () => {
+      test.it("should return 0 until read method has finished", () => {
+        test.expect(queriedOrigin.read.getters.stats().error).to.equal(0);
+      });
+
+      test.it("should return 1 when read has finished with an error", () => {
+        hasToReject = true;
+        return queriedOrigin.read().then(
+          () => {
+            return test.assert.fail();
+          },
+          () => {
+            return test.expect(queriedOrigin.read.getters.stats().error).to.equal(1);
+          }
+        );
+      });
+
+      test.it(
+        "should return 4 when read has finished with error and it has been called 4 times",
+        () => {
+          hasToReject = true;
+          return Promise.all([
+            queriedOrigin.read(),
+            queriedOrigin.read(),
+            queriedOrigin.read(),
+            queriedOrigin.read()
+          ]).then(
+            () => {
+              return test.assert.fail();
+            },
+            () => {
+              return test.expect(queriedOrigin.read.getters.stats().error).to.equal(4);
             }
           );
         }
