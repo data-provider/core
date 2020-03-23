b10943835eabc0554a33e77896b9bc7e12652f31
javierbrea <javier.brea@gmail.com>
2020-03-23 18:01:55 +0100

    feat(react): Remove useRefresh unnecessary usage

--- basic-tutorial/src/modules/FilteredTodoList.js
+++ basic-tutorial/src/modules/FilteredTodoList.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { useData, useLoading, useRefresh } from "@data-provider/react";
+import { useData, useLoading } from "@data-provider/react";
 
 import { todosFiltered, updateTodo } from "../data/todos";
 import TodoList from "../components/TodoList";
 
 const FilteredTodoList = ({ showCompleted }) => {
   const todosProvider = todosFiltered.query({ completed: showCompleted });
-  useRefresh(todosProvider);
   const todos = useData(todosProvider);
   const loading = useLoading(todosProvider);
 
