a852f922605cab7b09181f3b0a2e6d8071af70ff
javierbrea <javier.brea@gmail.com>
2020-05-02 12:28:57 +0200

    test(stryker): Add stryker config

---
+++ stryker.conf.js
@@ -0,0 +1,19 @@
+/**
+ * @type {import('@stryker-mutator/api/core').StrykerOptions}
+ */
+module.exports = {
+  mutator: "javascript",
+  files: ["*.js", "src/**/*.js", "test/**/*.js"],
+  packageManager: "npm",
+  dashboard: {
+    project: "github.com/data-provider/core",
+    version: "master",
+  },
+  reporters: ["html", "clear-text", "progress", "dashboard"],
+  testRunner: "jest",
+  transpilers: ["babel"],
+  coverageAnalysis: "off",
+  babel: {
+    optionsFile: ".babelrc",
+  },
+};
